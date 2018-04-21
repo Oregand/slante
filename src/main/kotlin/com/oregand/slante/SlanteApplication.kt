2f32b9b231eeb7d3e101e3e48b3253cca7caa509
David O'Regan <doregan@vsware.ie>
2018-04-21 19:40:04 +0100

    [nostory] Batman! This commit has no parents

---
+++ src/main/kotlin/com/oregand/slante/SlanteApplication.kt
@@ -0,0 +1,11 @@
+package com.oregand.slante
+
+import org.springframework.boot.autoconfigure.SpringBootApplication
+import org.springframework.boot.runApplication
+
+@SpringBootApplication
+class SlanteApplication
+
+fun main(args: Array<String>) {
+    runApplication<SlanteApplication>(*args)
+}
---
+++ src/test/kotlin/com/oregand/slante/SlanteApplicationTests.kt
@@ -0,0 +1,16 @@
+package com.oregand.slante
+
+import org.junit.Test
+import org.junit.runner.RunWith
+import org.springframework.boot.test.context.SpringBootTest
+import org.springframework.test.context.junit4.SpringRunner
+
+@RunWith(SpringRunner::class)
+@SpringBootTest
+class SlanteApplicationTests {
+
+	@Test
+	fun contextLoads() {
+	}
+
+}
